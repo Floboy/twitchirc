425144e73db3e8609ded16c039d7d79fe0cec6b0
Mm2PL <jakis128@gmail.com>
2019-04-28 19:00:01 +0200

    PyPi-fied

---
+++ irclib/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='irc-lib',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='GPLv3',
+    author='maciek',
+    author_email='',
+    description='Library for twitch irc.'
+)
