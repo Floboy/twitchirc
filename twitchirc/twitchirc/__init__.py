61d0db9a7d7318e88f883ea4f203f8c6e49c0fff
Mm2PL <jakis128@gmail.com>
2019-05-26 22:49:16 +0200

    Licence, logging

--- twitchirc/twitchirc/__init__.py
+++ twitchirc/twitchirc/__init__.py
@@ -1,3 +1,18 @@
+#  Library to make crating bots for Twitch chat easier.
+#  Copyright (c) 2019 Maciej Marciniak
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 from .patterns import *
 from .messages import ChannelMessage, PartMessage, Message, JoinMessage, NoticeMessage, PingMessage, auto_message, \
     process_twitch_flags, PongMessage
@@ -6,7 +21,7 @@ from .command import Command
 from .bot import Bot
 
 from .bot_storage import JsonStorage, CannotLoadError, AmbiguousSaveError
-from .log import info, warn, log
+from .log import info, warn, log, log_file, log_rotate_delay, rotate_logs, enable_file_logging
 from .permissions import require_permission, auto_group, PermissionList
 from .permission_names import *
 from .stock_commands import get_no_permission_generator, get_quit_command, get_part_command, get_join_command, get_perm_command
