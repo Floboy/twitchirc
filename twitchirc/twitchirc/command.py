8ede8411f5a079f7928b3d2088f7be48b420f3e5
Mm2PL <jakis128@gmail.com>
2019-09-13 18:17:40 +0200

    Add limit_to_channels to Command.

--- twitchirc/twitchirc/command.py
+++ twitchirc/twitchirc/command.py
@@ -12,6 +12,14 @@
 #
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import typing
 
@@ -24,7 +32,8 @@ class Command:
     def __init__(self, chat_command: str, function: typing.Callable, parent: Bot,
                  forced_prefix: typing.Optional[str] = None, enable_local_bypass: bool = True,
                  matcher_function: typing.Optional[typing.Callable[[twitchirc.ChannelMessage,
-                                                                    typing.Any], bool]] = None):
+                                                                    typing.Any], bool]] = None,
+                 limit_to_channels: typing.Optional[typing.List[str]] = None):
         self.matcher_function = matcher_function
         self.enable_local_bypass = enable_local_bypass
         self.ef_command = (forced_prefix + chat_command + ' ') if forced_prefix is not None else chat_command + ' '
@@ -33,10 +42,13 @@ class Command:
         self.permissions_required = []
         self.forced_prefix = forced_prefix
         self.parent = parent
+        self.limit_to_channels = limit_to_channels
 
     def __call__(self, message: twitchirc.ChannelMessage):
         if self.permissions_required:
             o = self.parent.check_permissions_from_command(message, self)
-            if o:  # a non empty list of missing permissions.
+            if o:  # a non-empty list of missing permissions.
                 return
+        if self.limit_to_channels is not None and message.channel not in self.limit_to_channels:
+            return
         self.function(message)
